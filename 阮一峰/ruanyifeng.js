8f56a7dd4a835e4eedeaa58edd2da7985c06f7a1
ROLYPOLY111 <huangjie1234huang@qq.com>
2020-04-25 09:59:47 +0800

    阮一峰

---
+++ 阮一峰/ruanyifeng.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+//读取图片
+// fs.readFile('./imgs/小丑Mac.jpg',(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         // console.log(data.length+'bytes')
+//         // console.log(data.toString('utf-8'))
+//     }
+// })
+
+//读取文件信息
+// fs.stat('./imgs/小丑Mac.jpg',(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(data)
+//     }
+// })
+
---
+++ 阮一峰/stream.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+//流
+//创建一个流
+// let readtxt = fs.ReadStream('./input.txt', 'utf-8')
+
+// //表示读取
+// readtxt.on('data', (chunk) => {
+//     console.log(chunk)
+// })
+
+// //表示流到了末尾
+// readtxt.on('end', () => {
+//     console.log('结束')
+// })
+
+// //error表示出错
+// readtxt.on('error', (err) => {
+//     console.log(err)
+// })
+
+
