b14fe9176e97259708a6f7d5c248469e09eba219
zouHuaLu <huangjie1234huang@qq.com>
2021-10-25 15:16:51 +0800

    07.Buffer

--- 01.http模块/01app.js
+++ 01.http模块/01app.js
@@ -1,15 +1,17 @@
 var http = require('http');
 const url = require('url')
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+// http.createServer(function (request, response) {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
 
-  // 获取浏览器访问的地址
-  if(request.url != '/favicon.ico'){
-    var userinfo = url.parse(request.url,true).query
-    console.log(userinfo);
-  }
+//   // 获取浏览器访问的地址
+//   if(request.url != '/favicon.ico'){
+//     var userinfo = url.parse(request.url,true).query
+//     console.log(userinfo);
+//   }
 
-  response.end('Hello World');
-}).listen(8081);
+//   response.end('Hello World');
+// }).listen(8081);
 
-console.log('服务运行于： http://127.0.0.1:8081/');
+// console.log('服务运行于： http://127.0.0.1:8081/');
+
+console.log(http.globalAgent)
